c63f7a1a08bdf43dd7d6d9a1103eea4b196923bb
happyLcj <1602231069@qq.com>
2015-11-13 21:00:36 +0800

    flask-wtf used

--- hello.py
+++ hello.py
@@ -3,16 +3,31 @@ from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
 from datetime import datetime
+from flask.ext.wtf import Form
+from wtforms import StringField,SubmitField,TextAreaField
+from wtforms.validators import Required
+from wtforms.validators import Email
+
+
+
 app=Flask(__name__)
+app.config['SECRET_KEY']='I guess you can see'
 
 moment=Moment(app)
 manager = Manager(app)
 bootstrap=Bootstrap(app)
+class NameForm(Form):
+    name=StringField('What is your name?',validators=[Required()])
+    submit=SubmitField('Submit')
 
-
-@app.route('/')
+@app.route('/',methods=["GET","POST"])
 def index():
-    return render_template('index.html',current_time = datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ""
+    return render_template('index.html',name=name, form=form, current_time = datetime.utcnow())
 #current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
