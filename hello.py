ed314a3ab6fff467db66c901c20c35057d5f33d3
happyLcj <1602231069@qq.com>
2015-11-14 14:44:09 +0800

    add SQL

--- hello.py
+++ hello.py
@@ -7,14 +7,38 @@ from flask.ext.wtf import Form
 from wtforms import StringField,SubmitField,TextAreaField
 from wtforms.validators import Required
 from wtforms.validators import Email
+from flask.ext.sqlalchemy import SQLAlchemy
+import os
+basedir=os.path.abspath(os.path.dirname(__file__))
 
 app=Flask(__name__)
 app.config['SECRET_KEY']='I guess you can see'
+
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN']=True
+db=SQLAlchemy(app)
 
 moment=Moment(app)
 manager = Manager(app)
 bootstrap=Bootstrap(app)
 
+class Role(db.Model):
+   __tablename__='roles'
+   id=db.Column(db.Integer,primary_key=True)
+   name=db.Column(db.String(64),unique=True)
+   users=db.relationship('User',backref='role')
+   def __repr__(self):
+       return '<Role%r>' % self.name
+
+class User(db.Model):
+    __tablename__='user'
+    id=db.Column(db.Integer,primary_key=True)
+    username=db.Column(db.String(64),unique=True,index=True)
+    role_id=db.Column(db.Integer,db.ForeignKey('roles.id'))
+    def __repr__(self):
+        return '<User%r>' % self.username
+        
+        
 class NameForm(Form):
     name=StringField('What is your name?',validators=[Required()])
     submit=SubmitField('Submit')
