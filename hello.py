b648c65acb5de8c6f831bd3de9d0222142d01360
happyLcj <1602231069@qq.com>
2015-11-14 16:10:16 +0800

    operators of SQL and Flask-Migrate

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
@@ -8,7 +9,9 @@ from wtforms import StringField,SubmitField,TextAreaField
 from wtforms.validators import Required
 from wtforms.validators import Email
 from flask.ext.sqlalchemy import SQLAlchemy
-import os
+from flask.ext.script import Shell
+from flask.ext.migrate import Migrate,MigrateCommand
+
 basedir=os.path.abspath(os.path.dirname(__file__))
 
 app=Flask(__name__)
@@ -22,11 +25,14 @@ moment=Moment(app)
 manager = Manager(app)
 bootstrap=Bootstrap(app)
 
+migrate=Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
 class Role(db.Model):
    __tablename__='roles'
    id=db.Column(db.Integer,primary_key=True)
    name=db.Column(db.String(64),unique=True)
-   users=db.relationship('User',backref='role')
+   users=db.relationship('User',backref='role',lazy='dynamic')
    def __repr__(self):
        return '<Role%r>' % self.name
 
@@ -47,15 +53,21 @@ class NameForm(Form):
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get("name")
-        if old_name is not None and old_name != form.name.data:
-            flash("looks like you have changed your name !")
-        session['name']=form.name.data;
+        user=User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user=User(username=form.name.data)
+            db.session.add(user)
+            session['known']=False
+        else:
+            session['known']=True
+        session['name']=form.name.data
+        form.name.data=''
         return redirect(url_for("index"))
     return render_template(
         'index.html',
         name=session.get('name'),
         form=form,
+        known=session.get('known',False),
         current_time = datetime.utcnow()
     )
 
@@ -70,6 +82,9 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
    return render_template('500.html'),500
+def make_shell_context():
+    return dict(app=app,db=db,User=User,Role=Role)
+manager.add_command("shell",Shell(make_context=make_shell_context))
 
 if __name__=='__main__':
     manager.run()
